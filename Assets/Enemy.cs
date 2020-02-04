1e6cca3e8d4c2bd350039c877d0b569c0227f491
Jamila <Jama@Mikhailas-MacBook-Pro.local>
2020-02-04 11:27:44 -0800

    combat added, working on combat animation.

---
+++ Assets/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public int health;
+   
+   
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("isRunning", true);
+    }
+
+    void Update()
+    {
+
+        if (health <= 0)
+
+        {
+            Debug.Log("hp loss!");
+            Destroy(gameObject);
+        }
+
+    }
+
+    public void TakeDamage(int damage)
+
+    {
+        health -= damage;
+        Debug.Log("damage taken!");
+    }
+
+}
