c7c28079f5655e65c76f724dc038bd2af08c4014
Student <example@example.com>
2020-02-05 15:56:44 -0800

    add winning condition and restart level after win

---
+++ Assets/WinFlag.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinFlag : MonoBehaviour
+{
+    public GameObject youWinText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name.Equals("Player"))
+        {
+            slowTime(.07f);
+            youWinText.SetActive(true);
+        }
+    }
+
+    void slowTime(float speed)      
+    { 
+        Time.timeScale = speed;
+        StartCoroutine(Reset(.2f));
+    }
+
+    private IEnumerator Reset(float reset)
+    {
+        yield return new WaitForSeconds (reset);
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("Prototype_1");
+    }
+
+}
