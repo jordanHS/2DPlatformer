c48c8942623828ab22d9da39663a96b448831a3e
Jamila <Jama@Mikhailas-MacBook-Pro.local>
2020-02-06 10:50:45 -0800

    combat animation.

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -5,21 +5,23 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    private float timeBtwAttack;
-    public float startTimeBtwAttack;
+    private float timeBtwAttack; // prvent player from spam attack
+    public bool combat; // trigger for combat anim
+    public Animator animator; // anim to combat
+    public float startTimeBtwAttack; //default value
     public Transform attackPos; //circle position
     public LayerMask whatIsEnemies; //the only enemies are attacked instead of things like envi
     public float attackRange; //circle range
     public int damage;
-    void Update()
+    void Update() 
     {
+        Attack();
         if (timeBtwAttack <= 0)
         {
             if (Input.GetKey(KeyCode.Tab))
             {
                 timeBtwAttack = startTimeBtwAttack;
                
-
                 Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position,
                 attackRange, whatIsEnemies);
 
@@ -33,6 +35,30 @@ public class PlayerAttack : MonoBehaviour
         {
             timeBtwAttack -= Time.deltaTime;
         }
+    }
+    void FixedUpdate()
+    {
+        HandleAttacks();
+        ResetValues();
+    }
+    public void HandleAttacks()
+    {
+        if (combat)
+        {
+            animator.SetTrigger("combat");
+        }
+
+    }
+     void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            animator.SetTrigger("Combat");
+        }
+    }
+    private void ResetValues()
+    {
+        combat = false;
     }
     void OnDrawGizmosSelected()
     {
